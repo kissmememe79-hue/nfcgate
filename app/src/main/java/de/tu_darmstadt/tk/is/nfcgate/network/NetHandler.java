c5ec9f3f66375a4c7f63e09873c75acb3f3cb633
kissmememe79-hue <kissmememe79@gmail.com>
2026-01-13 21:09:50 +0000

    Forge my Private Relay App

---
+++ app/src/main/java/de/tu_darmstadt/tk/is/nfcgate/network/NetHandler.java
@@ -0,0 +1,5 @@
+package de.tu_darmstadt.tk.is.nfcgate.network;
+public class NetHandler {
+    public static final String hostname = "curly-space-goldfish-r45p7vxrq4g43x97v-5566.app.github.dev";
+    public static final int port = 443;
+}
